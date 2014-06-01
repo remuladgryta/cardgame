5f228b7b4f921e8ddc46fb3d1b6da87493cb31c1
remuladgryta <remuladgryta@gmail.com>
2014-06-01 21:41:30 +0200

    Formatting.

--- src/com/remuladgryta/cardgame/GameMap.java
+++ src/com/remuladgryta/cardgame/GameMap.java
@@ -20,39 +20,44 @@ import com.remuladgryta.hex.TileCoord;
 public class GameMap {
 	HashMap<CubeCoord, List<Entity>> tiles;
 	GameEngine gameEngine;
-	public GameMap(GameEngine engine){
+
+	public GameMap(GameEngine engine) {
 		tiles = new HashMap<CubeCoord, List<Entity>>();
 		gameEngine = engine;
 	}
-	
-	public GameMap(GameEngine engine, TileCoord size){
+
+	public GameMap(GameEngine engine, TileCoord size) {
 		this(engine);
-		for(int p =0; p<=size.getP(); p++){
-			for(int q =0; q<=size.getQ(); q++){
-				tiles.put(HexMath.cubeFromTile(new TileCoord(p,q)), new LinkedList<Entity>());
+		for (int p = 0; p <= size.getP(); p++) {
+			for (int q = 0; q <= size.getQ(); q++) {
+				tiles.put(HexMath.cubeFromTile(new TileCoord(p, q)),
+						new LinkedList<Entity>());
 			}
 		}
 	}
-	
-	public GameMap(GameEngine engine, int radius){
+
+	public GameMap(GameEngine engine, int radius) {
 		this(engine);
-		List<CubeCoord> tileList = HexMath.inRange(new CubeCoord(0,0,0), radius);
-		for(CubeCoord tile : tileList){
+		List<CubeCoord> tileList = HexMath.inRange(new CubeCoord(0, 0, 0),
+				radius);
+		for (CubeCoord tile : tileList) {
 			tiles.put(tile, new LinkedList<Entity>());
 		}
 	}
-	
-	public GameEngine getEngine(){
+
+	public GameEngine getEngine() {
 		return gameEngine;
 	}
-	
+
 	/**
 	 * 
 	 * @param coord
-	 * @return an immutable list of the current entities at a given tile. will not update to reflect changes on the map
+	 * @return an immutable list of the current entities at a given tile. will
+	 *         not update to reflect changes on the map
 	 */
-	public List<Entity> entitiesAt(CubeCoord coord){
-		return Collections.unmodifiableList(new ArrayList<Entity>(tiles.get(coord)));
+	public List<Entity> entitiesAt(CubeCoord coord) {
+		return Collections.unmodifiableList(new ArrayList<Entity>(tiles
+				.get(coord)));
 	}
 
 	public void moveEntity(Entity e, CubeCoord target) {
@@ -67,11 +72,12 @@ public class GameMap {
 					"Entity tried to move to full tile");
 		}
 	}
-	
-	public void removeEntity(CubeCoord coord, Entity e){
+
+	public void removeEntity(CubeCoord coord, Entity e) {
 		tiles.get(coord).remove(e);
 		e.setLocation(null);
-		gameEngine.getEventDispatch().dispatch(new MapRenderStateChangedEvent());
+		gameEngine.getEventDispatch()
+				.dispatch(new MapRenderStateChangedEvent());
 	}
 
 	public void addEntity(CubeCoord coord, Entity e) {
@@ -84,27 +90,28 @@ public class GameMap {
 			throw new RuntimeException("Tried to add entity to full tile");
 		}
 	}
-	
-	public void addEntityAtRandom(Entity e){
+
+	public void addEntityAtRandom(Entity e) {
 		Set<CubeCoord> mapTiles = tiles.keySet();
 		int index = gameEngine.getRand().nextInt(mapTiles.size());
 		Iterator<CubeCoord> iter = mapTiles.iterator();
-		CubeCoord c=null;
-		for(int i=0;i<=index;i++){
-			c=iter.next();
+		CubeCoord c = null;
+		for (int i = 0; i <= index; i++) {
+			c = iter.next();
 		}
-		addEntity(c,e);
+		addEntity(c, e);
 	}
-	
+
 	/**
 	 * Note: Modifying the returned set will result in unpredictable behavior
+	 * 
 	 * @return
 	 */
-	public Set<Entry<CubeCoord, List<Entity>>> getTileData(){
+	public Set<Entry<CubeCoord, List<Entity>>> getTileData() {
 		return Collections.unmodifiableSet(tiles.entrySet());
 	}
-	
-	public Set<CubeCoord> getTiles(){
+
+	public Set<CubeCoord> getTiles() {
 		return Collections.unmodifiableSet(tiles.keySet());
 	}
 
