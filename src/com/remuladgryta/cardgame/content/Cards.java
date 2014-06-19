188fd5e24fd437e40622d2102de8b872be414a12
remuladgryta <remuladgryta@gmail.com>
2014-06-19 02:39:43 +0200

    Formatting.

--- src/com/remuladgryta/cardgame/content/Cards.java
+++ src/com/remuladgryta/cardgame/content/Cards.java
@@ -8,9 +8,10 @@ import com.remuladgryta.cardgame.filter.FilterIdentity;
 import com.remuladgryta.cardgame.filter.TileFilter;
 
 public class Cards {
+	public static Card debug(){return new Card("dummy", "desc", Arrays.asList(new ICardEffect[]{CardEffects.WALL()}), new FilterIdentity()); }
 	public static Card move(){ return new Card("Move", "Move to an adjacent tile", Arrays.asList(new ICardEffect[]{CardEffects.MOVE, CardEffects.DISCARD}), TileFilters.FILTER_ADJACENT);}
-	 public static Card attack(){ return new Card("Attack", "Deal 2 damage to an adjacent tile", Arrays.asList(new ICardEffect[]{CardEffects.DAMAGE(2), CardEffects.DISCARD}), TileFilters.FILTER_ADJACENT);}
-	 public static Card insult(){ return new Card("Insult","Deal 1 damage to any tile <br/><br/><i>Words hurt too. Just not as much as sticks and stones.</i>",Arrays.asList(new ICardEffect[]{CardEffects.DAMAGE(1), CardEffects.DISCARD}),new FilterIdentity());}
-	 public static Card egocentricity(){ return new Card("Egocentricity", "Rotate tiles adjacent to you one step clockwise <br/><br/><i>The world revolves around me! </i>", Arrays.asList(new ICardEffect[]{CardEffects.ROTATE(1),CardEffects.DISCARD}),TileFilters.FILTER_SELF);}
-	 public static Card debug(){return new Card("dummy", "desc", Arrays.asList(new ICardEffect[]{CardEffects.WALL}), new FilterIdentity()); }
+	public static Card attack(){ return new Card("Attack", "Deal 2 damage to an adjacent tile", Arrays.asList(new ICardEffect[]{CardEffects.DAMAGE(2), CardEffects.DISCARD}), TileFilters.FILTER_ADJACENT);}
+	public static Card insult(){ return new Card("Insult","Deal 1 damage to any tile <br/><br/><i>Words hurt too. Just not as much as sticks and stones.</i>",Arrays.asList(new ICardEffect[]{CardEffects.DAMAGE(1), CardEffects.DISCARD}),new FilterIdentity());}
+	public static Card egocentricity(){ return new Card("Egocentricity", "Rotate tiles adjacent to you one step clockwise <br/><br/><i>The world revolves around me! </i>", Arrays.asList(new ICardEffect[]{CardEffects.ROTATE(1),CardEffects.DISCARD}),TileFilters.FILTER_SELF);}
+	 
 }
